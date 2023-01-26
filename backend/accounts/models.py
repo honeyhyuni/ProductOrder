884b7768ed51e2a01f88187c4f64dad33fabe107
honeyhyuni <tjfkqwkd001@naver.com>
2023-01-26 20:29:35 +0900

    StratProject_and_makeUserModel

---
+++ backend/accounts/models.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from .managers import UserManager
+
+
+#  장고 기본 User 모델 상속
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(_('email address'), unique=True)
+    name = models.CharField(max_length=10)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        db_table = 'user'
