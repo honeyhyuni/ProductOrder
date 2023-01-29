cc20d87f42629d75ccd8a6cbd2f5e11c89a1f2e2
honeyhyuni <tjfkqwkd001@naver.com>
2023-01-29 17:24:46 +0900

    add CouponModels

--- backend/coupon/admin.py
+++ backend/coupon/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Coupon, CouponRules
 
-# Register your models here.
+
+@admin.register(CouponRules)
+class UserCouponAdmin(admin.ModelAdmin):
+    list_display = ('coupon_name', 'discount_policy', 'discount', 'created_at',)
+    list_display_links = ('coupon_name',)
+
+
+@admin.register(Coupon)
+class CouponAdmin(admin.ModelAdmin):
+    list_display = ('coupon_rules', 'coupon_code', 'start_date', 'end_date', 'active')
+    list_display_links = ('coupon_code',)
--- backend/coupon/models.py
+++ backend/coupon/models.py
@@ -1,3 +1,79 @@
 from django.db import models
+from django.core.validators import ValidationError
 
-# Create your models here.
+
+class TimestampedModel(models.Model):
+    """
+        생성일자, 수정일자 Model
+    """
+    created_at = models.DateTimeField('Created AT', auto_now_add=True)
+    updated_at = models.DateTimeField('Updated AT', auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class CouponRules(TimestampedModel):
+    """
+        쿠폰 BASE Model
+    """
+    Percent_discount = 'PD'
+    FlatRate_discount = 'FD'
+    Discount_Choice = [
+        (Percent_discount, 'Percent'),
+        (FlatRate_discount, 'FlatRate'),
+    ]
+
+    coupon_name = models.CharField("Name", max_length=100)
+    discount_policy = models.CharField("Discount Policy",
+                                       max_length=2,
+                                       choices=Discount_Choice,
+                                       default=FlatRate_discount,
+                                       )
+    discount = models.PositiveIntegerField("Discount", help_text='if select PD 5%~100%, else 1000~50000')
+
+    def clean(self):
+        """
+            유효성 검사
+        """
+        discount_policy = self.discount_policy
+        discount = self.discount
+        if discount_policy == 'PD' and (discount % 5 != 0 or not (5 <= discount <= 100)):
+            raise ValidationError("%할인은 5의배수이면서 100 이하입니다")
+        elif discount_policy == 'FD' and (discount % 5000 != 0 or not (5000 <= discount <= 50000)):
+            raise ValidationError("정액할인은 5000 배수이면서 50000원 이하입니다.")
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['discount_policy', 'discount'], name='Base coupon is only one')
+        ]
+        verbose_name = '쿠폰 베이스'
+        verbose_name_plural = '쿠폰 베이스 모음'
+        db_table = 'coupon_rules'
+        ordering = ('-created_at',)
+
+    def __str__(self):
+        return self.coupon_name
+
+
+class Coupon(models.Model):
+    """
+        등록된 쿠폰 Model
+    """
+    coupon_rules = models.ForeignKey(CouponRules, on_delete=models.CASCADE)
+    coupon_code = models.CharField("Coupon Code", max_length=15, unique=True,
+                                   help_text='only between 10 and 15 of characters',
+                                   editable=False)
+    start_date = models.DateTimeField('Start DT', auto_now_add=True)
+    end_date = models.DateTimeField('End DT', null=True, blank=True)
+    active = models.BooleanField("active", default=True, null=True)
+
+    # activate 필드 (User 에게) True - 등록전, False - 등록후 Null - 만기 완료
+    class Meta:
+        verbose_name = '등록된 쿠폰'
+        verbose_name_plural = '등록된 쿠폰 모음'
+        db_table = 'coupon'
+        ordering = ('-start_date',)
+
+    def __str__(self):
+        return f"{self.coupon_code[:5]}"
