3d91a888537542b96a2a1e44ede0c71b51a9679f
honeyhyuni <tjfkqwkd001@naver.com>
2023-02-07 18:04:16 +0900

    Login, Logout

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,1 +1,7 @@
-urlpatterns=[]
+
+from django.urls import path, include
+
+urlpatterns = [
+    path('signup/', include('dj_rest_auth.registration.urls')),
+    path('', include('dj_rest_auth.urls')),  # 로그인, 로그아웃, TokenRefresh, changePassword ETC
+]
