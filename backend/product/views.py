9d9986a9bfbdd18e9d175f29a589ddf9ee30065c
honeyhyuni <tjfkqwkd001@naver.com>
2023-03-13 18:30:55 +0900

    Travis Test

--- backend/product/views.py
+++ backend/product/views.py
@@ -6,7 +6,6 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
-
 from coupon.models import UserCoupon
 from coupon.views import PostPageNumberPagination
 from order.serializers import OrderSerializer
